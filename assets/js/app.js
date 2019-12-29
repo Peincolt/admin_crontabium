fa6331ea5ff392511e8eac78d43598c95315186e
Peincolt <Peincolt@users.noreply.github.com>
2019-12-29 13:59:58 +0100

    Changement image suivant type unit et ajax 1er arrivée sur la page

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,22 @@ const axios = require('axios');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
 
+window.onload = function(){
+    select = document.getElementById('selectUnit');
+    id = select.value;
+    if (select.name == "ships") {
+        url = "/ship/";
+    } else {
+        url = "/hero/";
+    }
+
+    url+=id;
+
+    axios.post(url).then(response => {
+        constructList(response.data.name,response.data.players);
+    })
+}
+
 document.getElementById('selectUnit').addEventListener('change',function(event){
 
     document.getElementById('list-ajax').innerHTML="";
@@ -29,61 +45,60 @@ document.getElementById('selectUnit').addEventListener('change',function(event){
     url+=this.value;
 
     axios.post(url).then(response => {
-
-        console.log(response);
-        div = document.getElementById('list-ajax');
-        title = document.createElement('h2');
-        table = document.createElement('table');
-        thead = document.createElement('thead');
-        trhead = document.createElement('tr');
-        thName = document.createElement('th');
-        thLevel = document.createElement('th');
-        thGalacticalPower = document.createElement('th');
-        thRarity = document.createElement('th');
-        tbody = document.createElement('tbody');
-
-        table.classList.add('table','table-hover');
-        title.classList.add('h2');
-
-        title.appendChild(document.createTextNode(response.data.name));
-        thName.appendChild(document.createTextNode('Player'));
-        thLevel.appendChild(document.createTextNode('Level'));
-        thRarity.appendChild(document.createTextNode('Stars'));
-        thGalacticalPower.appendChild(document.createTextNode('Galactical Power'));
-
-        table.appendChild(thead);
-        thead.appendChild(trhead);
-        trhead.appendChild(thName);
-        trhead.appendChild(thLevel);
-        trhead.appendChild(thRarity);
-        trhead.appendChild(thGalacticalPower);
-        table.appendChild(tbody);
-
-        for (i=0;i<response.data.players.length;i++) {
-            tr = document.createElement('tr');
-            tdName = document.createElement('td');
-            tdLevel = document.createElement('td');
-            tdGalacticalPower = document.createElement('td');
-            tdRarity = document.createElement('td');
-
-            tdName.appendChild(document.createTextNode(response.data.players[i].player_name));
-            tdLevel.appendChild(document.createTextNode(response.data.players[i].level));
-            tdRarity.appendChild(document.createTextNode(response.data.players[i].stars));
-            tdGalacticalPower.appendChild(document.createTextNode(response.data.players[i].galactical_puissance));
-
-            tr.appendChild(tdName);
-            tr.appendChild(tdLevel);
-            tr.appendChild(tdRarity);
-            tr.appendChild(tdGalacticalPower);
-
-            tbody.appendChild(tr);
-
-        }
-
-        console.log(table);
-        console.log(div);
-        div.appendChild(title);
-        div.appendChild(table);
+        constructList(response.data.name,response.data.players);
     })
-
 });
+
+function constructList(name, data) {
+    div = document.getElementById('list-ajax');
+    title = document.createElement('h2');
+    table = document.createElement('table');
+    thead = document.createElement('thead');
+    trhead = document.createElement('tr');
+    thName = document.createElement('th');
+    thLevel = document.createElement('th');
+    thGalacticalPower = document.createElement('th');
+    thRarity = document.createElement('th');
+    tbody = document.createElement('tbody');
+
+    table.classList.add('table','table-hover');
+    title.classList.add('h2');
+
+    title.appendChild(document.createTextNode(name));
+    thName.appendChild(document.createTextNode('Player'));
+    thLevel.appendChild(document.createTextNode('Level'));
+    thRarity.appendChild(document.createTextNode('Stars'));
+    thGalacticalPower.appendChild(document.createTextNode('Galactical Power'));
+
+    table.appendChild(thead);
+    thead.appendChild(trhead);
+    trhead.appendChild(thName);
+    trhead.appendChild(thLevel);
+    trhead.appendChild(thRarity);
+    trhead.appendChild(thGalacticalPower);
+    table.appendChild(tbody);
+
+    for (i=0;i<data.length;i++) {
+        tr = document.createElement('tr');
+        tdName = document.createElement('td');
+        tdLevel = document.createElement('td');
+        tdGalacticalPower = document.createElement('td');
+        tdRarity = document.createElement('td');
+
+        tdName.appendChild(document.createTextNode(data[i].player_name));
+        tdLevel.appendChild(document.createTextNode(data[i].level));
+        tdRarity.appendChild(document.createTextNode(data[i].stars));
+        tdGalacticalPower.appendChild(document.createTextNode(data[i].galactical_puissance));
+
+        tr.appendChild(tdName);
+        tr.appendChild(tdLevel);
+        tr.appendChild(tdRarity);
+        tr.appendChild(tdGalacticalPower);
+
+        tbody.appendChild(tr);
+
+    }
+
+    div.appendChild(title);
+    div.appendChild(table);
+}
