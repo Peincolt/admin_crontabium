6ed37f49e4023651faa4c60fa8cecf222aa40189
Peincolt <Peincolt@users.noreply.github.com>
2019-12-29 13:32:14 +0100

    Ajout ajax get ships/hero informations

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,7 +10,80 @@ imagesContext.keys().forEach(imagesContext);
 
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+const axios = require('axios');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+
+document.getElementById('selectUnit').addEventListener('change',function(event){
+
+    document.getElementById('list-ajax').innerHTML="";
+
+    if (this.name == "ships") {
+        url = "/ship/";
+    } else {
+        url = "/hero/";
+    }
+
+    url+=this.value;
+
+    axios.post(url).then(response => {
+
+        console.log(response);
+        div = document.getElementById('list-ajax');
+        title = document.createElement('h2');
+        table = document.createElement('table');
+        thead = document.createElement('thead');
+        trhead = document.createElement('tr');
+        thName = document.createElement('th');
+        thLevel = document.createElement('th');
+        thGalacticalPower = document.createElement('th');
+        thRarity = document.createElement('th');
+        tbody = document.createElement('tbody');
+
+        table.classList.add('table','table-hover');
+        title.classList.add('h2');
+
+        title.appendChild(document.createTextNode(response.data.name));
+        thName.appendChild(document.createTextNode('Player'));
+        thLevel.appendChild(document.createTextNode('Level'));
+        thRarity.appendChild(document.createTextNode('Stars'));
+        thGalacticalPower.appendChild(document.createTextNode('Galactical Power'));
+
+        table.appendChild(thead);
+        thead.appendChild(trhead);
+        trhead.appendChild(thName);
+        trhead.appendChild(thLevel);
+        trhead.appendChild(thRarity);
+        trhead.appendChild(thGalacticalPower);
+        table.appendChild(tbody);
+
+        for (i=0;i<response.data.players.length;i++) {
+            tr = document.createElement('tr');
+            tdName = document.createElement('td');
+            tdLevel = document.createElement('td');
+            tdGalacticalPower = document.createElement('td');
+            tdRarity = document.createElement('td');
+
+            tdName.appendChild(document.createTextNode(response.data.players[i].player_name));
+            tdLevel.appendChild(document.createTextNode(response.data.players[i].level));
+            tdRarity.appendChild(document.createTextNode(response.data.players[i].stars));
+            tdGalacticalPower.appendChild(document.createTextNode(response.data.players[i].galactical_puissance));
+
+            tr.appendChild(tdName);
+            tr.appendChild(tdLevel);
+            tr.appendChild(tdRarity);
+            tr.appendChild(tdGalacticalPower);
+
+            tbody.appendChild(tr);
+
+        }
+
+        console.log(table);
+        console.log(div);
+        div.appendChild(title);
+        div.appendChild(table);
+    })
+
+});
--- src/Controller/UnitController.php
+++ src/Controller/UnitController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Service\Entity\PlayerUnit;
 
 class UnitController extends AbstractController
@@ -26,7 +27,11 @@ class UnitController extends AbstractController
     public function units(Request $request, PlayerUnit $playerHelper)
     {
         $array = explode('_',$request->attributes->get('_route'));
-        $className = ucfirst(substr($array[1],0,strlen($array[1])-2));
+        if (stristr("ships",$array[1])) {
+            $className = ucfirst(substr($array[1],0,strlen($array[1])-1));
+        } else {
+            $className = ucfirst(substr($array[1],0,strlen($array[1])-2));
+        }
         $entityName = 'App\Entity\\'.$className;
         $number = $playerHelper->getNumberUnit($className);
 
@@ -48,13 +53,34 @@ class UnitController extends AbstractController
      */
     public function unit(Request $request, $id)
     {
+        $arrayReturn = array();
+        $array = explode('_',$request->attributes->get('_route'));
+        $entityName = ucfirst($array[1]);
+        $entityFullName = "App\Entity\\".$entityName;
+        $function = 'get'.$entityName.'Players';
         $unit = $this->getDoctrine()
         ->getManager()
-        ->getRepository(ucfirst($request->attributes->get('_route')))
+        ->getRepository($entityFullName)
         ->find($id);
+        $arrayReturn['name'] = $unit->getName();
+        $unitPlayers = $unit->$function();
 
-        return $this->render('unit/list.html.twig', [
+        for($i=0;$i<count($unitPlayers);$i++) {
+            $arrayReturn['players'][$i]['player_name'] = $unitPlayers[$i]->getPlayer()->getName();
+            $arrayReturn['players'][$i]['level'] = $unitPlayers[$i]->getLevel();
+            $arrayReturn['players'][$i]['stars'] = $unitPlayers[$i]->getNumberStars();
+            $arrayReturn['players'][$i]['galactical_puissance'] = $unitPlayers[$i]->getGalacticalPuissance();
+            if ($entityName == 'Hero') {
+                $arrayReturn['players'][$i]['relic'] = $unitPlayers[$i]->getRelicLevel();
+                $arrayReturn['players'][$i]['gear_level'] = $unitPlayers[$i]->getGearLevel();
+            }
+        }
+
+        $response = new JsonResponse($arrayReturn);
+
+        return $response;
+        /*return $this->render('unit/list.html.twig', [
             'unit' => $unit,
-        ]);
+        ]);*/
     }
 }
